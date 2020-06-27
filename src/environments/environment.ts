d111e6f8895e1151a86a14572127805bcb2448d0
Mohamed Arafath <35114431+mmsmz@users.noreply.github.com>
2020-06-28 01:15:24 +0530

    added the insert button and data is storing inside firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDfUjjMs2T8XzjyS7eoJfxZy4BHL9uADzM",
+    authDomain: "fir-crudapp-7ace5.firebaseapp.com",
+    databaseURL: "https://fir-crudapp-7ace5.firebaseio.com",
+    projectId: "fir-crudapp-7ace5",
+    storageBucket: "fir-crudapp-7ace5.appspot.com",
+    messagingSenderId: "189910818669",
+    appId: "1:189910818669:web:051f7507b02753e71dc464",
+    measurementId: "G-W04S79804V"
+  }
 };
 
 /*
